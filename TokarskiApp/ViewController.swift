7ed9a286da6bce360c006e76ced93aaafeed59f8
Mateusz T <mateuszt@imac.local>
2020-06-03 20:44:24 +0200

    Initial Commit

---
+++ TokarskiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TokarskiApp
+//
+//  Created by Mateusz T on 03.06.2020.
+//  Copyright © 2020 Mateusz T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
